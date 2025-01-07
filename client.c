964593941e996e5d0677eae1b37f9724bcf14d4c
ottobuff180 <obuff50@stuy.edu>
2025-01-07 14:15:32 -0500

    Copied over relevant code from lab 15

--- client.c
+++ client.c
@@ -8,6 +8,37 @@
 #include <errno.h>
 #include "client.h"
 
-int main(){
-  //Set up prompts
+int main() {
+  int to_server;
+  int to_client;
+  to_client = client_handshake( &to_server );
+  
+  while(1){
+    int ran;
+    int i = read(to_client,&ran,sizeof(ran));
+    if(i <= 0){
+      exit(1);
+    }
+    printf("%d\n", ran);
+  }
+}
+
+
+int client_handshake(int *to_server) {
+  char pp[100];
+  sprintf(pp,"%d",getpid());
+  printf("client 3: %s\n",pp);
+  mkfifo(pp, 0644);
+  *to_server = open(WKP, O_WRONLY);
+  write(*to_server,pp,sizeof(pp));
+  int downstream = open(pp, O_RDONLY);
+  printf("client 8\n");
+  unlink(pp);
+  int synack;
+  read(downstream,&synack,sizeof(synack));
+  printf("client 8: %d\n",synack);
+  synack++;
+  printf("client 8: %d\n",synack);
+  write(*to_server,&synack,sizeof(synack));
+  return downstream;
 }
--- server.c
+++ server.c
@@ -8,6 +8,84 @@
 #include <errno.h>
 #include "server.h"
 
-int main(){
-  //Set up random number of files
+int main() {
+  int to_client;
+  int from_client;
+  signal(SIGINT, signal_handler);
+  signal(SIGPIPE, SIG_IGN);
+  while(1){
+    from_client = server_handshake( &to_client );
+    while(1){
+      int ran = randomint();
+      int i = write(to_client,&ran,sizeof(ran));
+      if(i <= 0){
+        printf("client exited\n");
+        break;
+      }
+      sleep(1);
+    }
+    close(from_client);
+    close(to_client);
+  }
+}
+
+
+int server_setup() {
+  printf("server 1\n");
+  mkfifo(WKP, 0644);
+  printf("server 2\n");
+  int p1 = open(WKP, O_RDONLY);
+  unlink(WKP);
+  printf("server 4\n");
+  return p1;
+}
+
+int server_handshake(int *to_client) {
+  int from_client = server_setup();
+  char pp[100];
+  read(from_client,pp,sizeof(pp));
+  printf("server 5: %s\n",pp);
+  printf("server 6\n");
+  *to_client = open(pp, O_WRONLY);
+  int synack = getpid();
+  printf("server 7: %d\n",synack);
+  write(*to_client,&synack,sizeof(synack));
+  int finalack;
+  printf("server 9\n");
+  read(from_client,&finalack,sizeof(finalack));
+  printf("server 9: %d\n",finalack);
+  return from_client;
+}
+
+void server_handshake_half(int *to_client, int from_client) {
+  char pp[100];
+  read(from_client,pp,sizeof(pp));
+  printf("server 5: %s\n",pp);
+  printf("server 6\n");
+  *to_client = open(pp, O_WRONLY);
+  int synack = getpid();
+  printf("server 7: %d\n",synack);
+  write(*to_client,&synack,sizeof(synack));
+  int finalack;
+  printf("server 9\n");
+  read(from_client,&finalack,sizeof(finalack));
+  printf("server 9: %d\n",finalack);
+}
+
+int randomint(){
+  int r_file = open("/dev/random", O_RDONLY , 0);
+  
+  int buff;
+  int *p = &buff;
+  int bytes = read(r_file, p, sizeof(int));
+  
+  close(r_file);
+  
+  return buff;
+}
+
+
+int server_connect(int from_client) {
+  int to_client  = 0;
+  return to_client;
 }
